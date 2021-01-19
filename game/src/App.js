327aa211990a2b433ad96350a394a21184019b55
choisaelim <choisaelim@gmail.com>
2021-01-19 23:46:39 +0900

    board 추가

--- game/src/App.js
+++ game/src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
+import Board from  './page/Board';
 
 function App() {
   return (
     <div className="game">
         <div className="game-board">
-          {/* <Board /> */}
+          <Board/>
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
-        </div> 
+        </div>
       </div>
   );
 }
